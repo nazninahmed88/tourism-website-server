254d147c03fa02cd9a833cf06e99aaa870dd120f
nazninrinkie <nazninrinky@gmail.com>
2021-10-31 21:38:11 +0600

    'first'

--- index.js
+++ index.js
@@ -1,71 +1,113 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
-require('dotenv').config()
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zew5s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 console.log(uri)
-
-
 async function run() {
+    try {
+        await client.connect();
+        const database = client.db('tourisum');
+        const serviceCollections = database.collection('services');
 
-  try {
+      
 
-    await client.connect();
-   const database = client.db('tourisum');
-   const serviceCollection = database.collection('services');
-   const hotelsCollection = database.collection('hotels');
+        const hotelServices = database.collection('hotels');
+        const orderCollection = database.collection('orders');
+        const cartCollection = database.collection('orderCart');
 
-//  get Services
-   app.get('/services' , async(req , res )=>{
-    const cursor = serviceCollection.find({});
-      const services = await cursor.toArray();
-      res.send(services)
+        app.get('/hotels', async (req, res) => {
+            const cursor = hotelServices.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
 
-   })
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollections.find({});
+            const services = await cursor.toArray();
+            console.log(services);
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollections.findOne(query);
+            res.json(service);
 
+        })
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollections.deleteOne(query);
+            res.json(result);
+        })
 
-   app.get('/hotels' , async(req , res )=>{
-    const cursor = hotelsCollection.find({});
-      const hotels = await cursor.toArray();
-      res.send(hotels)
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            // console.log('hit the post api', service);
+            const result = await serviceCollections.insertOne(service);
+            // console.log(result);
+            res.json(result);
+        });
 
-   })
-    
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-    // const result = await foods.insertMany(docs, options);
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            order.createdAt = new Date();
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+        app.post('/cart', async (req, res) => {
+            const order = req.body;
+            const result = await cartCollection.insertOne(order);
+            res.json(result);
+        })
+        app.get('/cart', async (req, res) => {
+            const cursor = cartCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(typeof (id));
+            const query = { _id: id };
+            const result = await cartCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
 
-    // console.log(`${result.insertedCount} documents were inserted`);
-
-  }
-   finally {
-
-    // await client.close();
-
-  }
-
-}
-
+};
 run().catch(console.dir);
 
 
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World Tourisum!')
-})
-
+    res.send('Running tourisum');
+});
 app.listen(port, () => {
-  console.log('Example app listening at' , port)
-})
+    console.log('Running Port', port);
+});
